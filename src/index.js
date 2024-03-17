a95469526760f9b621dd63f74a6bea5f2d2879d9
Buntcz <testtigeryt@gmail.com>
2024-03-17 20:41:39 +0200

    --all

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Image from './images/Restaurant.png'
+
+const content = document.getElementById('content');
+
+function content() {
+    const header = document.createElement("h1");
+    header.textContent = 'The Crusty Crab';
+}
